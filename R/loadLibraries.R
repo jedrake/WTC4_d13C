c7d80a236c73bfb04b0507fdaf04a53b308943e8
jedrake <je.drake@uws.edu.au>
2016-08-12 14:24:56 +1000

    First commit. Added .gitignore to the repo (it ignores /Data and /Output). Added a script to load required packages ("loadLibraries.R").

---
+++ R/loadLibraries.R
@@ -0,0 +1,38 @@
+#----------------------------------------------------------------------------------------------------------------
+#- This script loads all of the required R libraries
+#----------------------------------------------------------------------------------------------------------------
+
+
+# do this once
+# devtools::install_bitbucket("remkoduursma/plotby")
+library(plotBy)
+
+#- little function to check for required libraries, and install them if required
+Library <- function(pkg, ...){
+  
+  PACK <- .packages(all.available=TRUE)
+  pkgc <- deparse(substitute(pkg))
+  
+  if(pkgc %in% PACK){
+    library(pkgc, character.only=TRUE)
+  } else {
+    install.packages(pkgc, ...)
+    library(pkgc, character.only=TRUE)
+  }
+  
+}
+
+#- load (and install if necessary) packages
+Library(dplyr)
+Library(stringr)
+Library(doBy)
+Library(plyr)
+Library(RColorBrewer)
+Library(reshape2)
+
+#- create directories, if needed
+#if(!dir.exists("output"))dir.create("output")
+#if(!dir.exists("cache"))dir.create("cache")
+
+#----------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------
